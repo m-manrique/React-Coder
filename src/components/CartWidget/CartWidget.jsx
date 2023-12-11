bb79f0b5ef57121c013e6a936224cd4570b8b5dc
Marlon Mauricio Manrique Meza <marlonmanrique1985@gmail.com>
2023-12-11 16:27:40 -0500

    Despliegue gh-pages V1

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,8 +2,8 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const CartWidget = () => {
   return (
-    <div>
-      <ShoppingCartIcon sx={{ fontSize: 40 }}/>
+    <div style={{ marginRight: 40}}>
+      <ShoppingCartIcon sx={{ fontSize: 40}}/>
       <span style={{ fontSize: 25}}>3</span>
     </div>
   );
